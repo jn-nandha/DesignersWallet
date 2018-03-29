52a5e6944cec07a4901dcb346ed6f64b742d84c1
chirag <chiragsolanki675@gmail.com>
2018-03-29 18:06:00 +0530

    Added Search master table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,29 +1,20 @@
 class ReportsController < ApplicationController
 	before_action :authenticate_admin! # here authenticate admin before action
-  skip_before_action :authenticate_user! # here skip authenticate user in all methods
+    skip_before_action :authenticate_user! # here skip authenticate user in all methods
 
 # def report
 # 	   # @users = User.all.paginate(:page => params[:page], :per_page => 5)
 # 	end
 
-	def new
-		
-	before_action :authenticate_admin! # here authenticate admin before action
-  skip_before_action :authenticate_user! # here skip authenticate user in all methods
-
-end
-
 	def report
-		 @user = Feedback.where(["design_id LIKE ?","%#{params[:search]}%"])
-		@feedback = Feedback.order("design_id")
+	    # @user = Feedback.where(["design_id LIKE ?","%#{params[:search]}%"])
+		@feedback = Feedback.all.order("design_id")
 		@cc = User.joins(:feedback)
 		@total_repoart = Feedback.select(:design_id).map(&:design_id).uniq
 		@ccuser = User.order("design_id")
+		@ccdesign = Design.order("design_id")
+		@deals = User.where(created_at: Date.today.all_day)
+		@cat = Category.all.order("cat_name ASC")
+		# @uacount = User.where("activation == true) 
 	end
-
-
-	# def create
-		
-	# end
-
 end

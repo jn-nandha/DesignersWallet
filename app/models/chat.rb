1b8706e33c223043959a4ee0a06685854fe1715a
tejasviniparikh <tejal0003@gmail.com>
2018-04-04 09:15:21 +0530

    local commits

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,8 +5,7 @@ class Chat < ApplicationRecord
   serialize :designs_id, Array
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
-  has_and_belongs_to_many :designs, dependent: :destroy
-
+ 
 	def designs
 		Design.where(id: Chat.find(self.id).designs_id)
 	end
